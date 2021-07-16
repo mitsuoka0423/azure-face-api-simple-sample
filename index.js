ef339452b993eace48545ac3a4156f305b0ef2f3
tmitsuoka0423 <tmitsuoka0423@gmail.com>
2021-07-16 23:54:42 +0900

    upd

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-'use strict';
+"use strict";
 
+// インストールしたライブラリを読み込む
 const { FaceClient } = require("@azure/cognitiveservices-face");
 const { CognitiveServicesCredentials } = require("@azure/ms-rest-azure-js");
 
-// Face APIパラメータ
+// Azure Face APIを使うための準備
 const faceKey = "キー1を入力する";
 const faceEndPoint = "エンドポイントを入力する";
-
 const cognitiveServiceCredentials = new CognitiveServicesCredentials(faceKey);
 const faceClient = new FaceClient(cognitiveServiceCredentials, faceEndPoint);
 
 const main = async () => {
-  const response = await faceClient.face.detectWithUrl('顔検出したい画像のURLを入力する');
-  console.log(response);
+  // Azure Face APIに画像を送り、結果を受け取る
+  const result = await faceClient.face.detectWithUrl("顔写真のURLを記入する", {
+    returnFaceAttributes: ["smile", "makeup"], // ここにカンマ(,)区切りでパラメータを追加する
+  });
+
+  console.log(result);
+  console.log(result[0].faceAttributes.makeup.eyeMakeup);
 };
 
 main();
